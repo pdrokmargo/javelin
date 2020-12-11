27bca5fbd844d0023fa00736018cde8b67ba8203
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-12-11 15:44:28 -0500

    ds

--- src/app/config.ts
+++ src/app/config.ts
@@ -7,7 +7,7 @@ function makeAppConfig() {
     brand: 'Javelin',
     user: 'Lisa',
     year,
-    version: 'v1.0.3',
+    version: 'v1.0.4',
     layoutBoxed: false,               // true, false
     navCollapsed: false,              // true, false
     navBehind: false,                 // true, false
