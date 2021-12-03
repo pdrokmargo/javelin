c144c2097aaad2aecc5266b8ea46a2a3c51b17c3
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-12-03 08:38:04 -0500

    Bump to version 1.0.15

--- src/app/config.ts
+++ src/app/config.ts
@@ -7,7 +7,7 @@ function makeAppConfig() {
     brand: 'Javelin',
     user: 'Lisa',
     year,
-    version: 'v1.0.13',
+    version: 'v1.0.15',
     layoutBoxed: false,               // true, false
     navCollapsed: false,              // true, false
     navBehind: false,                 // true, false
